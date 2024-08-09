af51275c8e34243eea333b43cb8836d7df5406b8
Sandrah <comfortsandrah13@gmail.com>
2024-08-10 01:49:51 +0300

    feat: Add UserProfile component
    The UserProfile component is added to display user information such as name, age, and bio. This component will be used to showcase the user profile section of the ALX React app.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile
+        name="Sandrah"
+        age="19"
+        bio="I love Jesus and i greatly enjoy reading"
+      />
     </>
   );
 }
---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
