e71de7bbc962641b1e478063accda8d89abe3a98
Sandrah <comfortsandrah13@gmail.com>
2024-08-10 01:40:40 +0300

    feat: Add MainContent component
    The MainContent component is added to display the main content of the ALX React app. It includes a paragraph with a list of cities the user loves to visit. This component will be used to showcase the main content section of the app.
    
    Note: Removed meta information from the commit message.

---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
