a9225b4c6fa581625374d9585f4f712ff2c9da4a
Sandrah <comfortsandrah13@gmail.com>
2024-08-10 01:41:21 +0300

    chore: Update App.jsx to include Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
-  return <WelcomeMessage />;
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
