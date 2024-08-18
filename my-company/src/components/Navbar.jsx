e0bc2e94ca399da7901c6026dd6688eb1af16aea
Sandrah <comfortsandrah13@gmail.com>
2024-08-18 23:55:13 +0300

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
+    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+    <ul style={{ display: 'flex', justifyContent: 'space-around', listStyleType: 'none', margin: 0, padding: 0 }}>
+      <li><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link></li>
+      <li><Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link></li>
+      <li><Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link></li>
+      <li><Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link></li>
+    </ul>
+  </nav>
   )
 }
 
