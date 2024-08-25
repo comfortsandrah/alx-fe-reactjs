900e2279e87624fc186ca77ce447f846991c4f28
Sandrah <comfortsandrah13@gmail.com>
2024-08-25 21:51:00 +0300

    update store function

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,9 +2,8 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) => {
-    setRecipes((state) => ({ recipes: [...state.recipes, recipe] }));
-  },
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
   // deleteRecipe: (id) => {
   //   set((state) => ({
   //     recipes: state.recipes.filter((recipe) => recipe.id !== id),
