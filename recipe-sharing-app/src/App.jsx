43208a38bd66f19236ba99691aaf46d10a6a6a41
Sandrah <comfortsandrah13@gmail.com>
2024-08-25 20:28:24 +0300

    Initialize the Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+function App() {
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,17 @@
+import useRecipeStore from "../Stores/useRecipeStore";
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default RecipeList;
