2552c5f4268fdc833256b46116239784d37cceb1
Sandrah <comfortsandrah13@gmail.com>
2024-09-07 01:12:04 +0300

    chore: Add error handling and refresh functionality to PostsComponent

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,20 +2,30 @@ import { useQuery } from "react-query";
 
 const fetchData = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
   return res.json();
 };
 
 const PostsComponent = () => {
-  const { data, error, isLoading } = useQuery("fetchData", fetchData);
+  const { data, isError, error, isLoading, refetch } = useQuery(
+    "fetchPosts",
+    fetchData,
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
 
-  if (isLoading) return
+  if (isLoading) return;
   <div>Loading...</div>;
 
-  if (error) return;
-  <div>Error handling data...</div>;
+  if (isError) return;
+  <div>Error handling data:{error?.message || "An unknown error occured"}</div>;
 
   return (
     <div>
+      <button onClick={refetch}>Refresh</button>
       {data.map((item) => (
         <div key={item.id}>{item.title}</div>
       ))}
