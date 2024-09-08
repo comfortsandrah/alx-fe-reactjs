1c1c9f51f846b7de4867e4f68b0e1ccd1f75401b
Sandrah <comfortsandrah13@gmail.com>
2024-09-08 23:50:48 +0300

    Refactor routing in App component to use "/profile/*" path for the Profile component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <Router>
       <Routes>
         <Route
-          path="/"
+          path="/profile/*"
           element={
             <ProtectedRoute isAuthenticated={isAuthenticated}>
               <Profile />
