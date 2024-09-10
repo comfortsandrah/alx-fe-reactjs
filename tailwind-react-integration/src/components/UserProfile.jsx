f2688d8323d8f8e69cce74f2c0e967ac7e00d009
Sandrah <comfortsandrah13@gmail.com>
2024-09-10 19:46:44 +0300

    Refactor UserProfile component for responsive design using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile max-w-xs bg-gray-100 p-4 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto" />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-sm md:text-base">
+    <div className="user-profile sm:max-w-xs bg-gray-100 sm:p-4 md:p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+      />
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
