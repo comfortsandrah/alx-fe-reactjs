f94fa3c913abfe145830cf6c2892dd9bd4f2a7d4
Sandrah <comfortsandrah13@gmail.com>
2024-09-10 23:27:23 +0300

    Building the Recipe Detail Page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
-    <>
-     <HomePage/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
