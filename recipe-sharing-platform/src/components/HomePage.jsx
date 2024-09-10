f49144b1fb6a4ed210dfa5ef05dd253e59614a61
Sandrah <comfortsandrah13@gmail.com>
2024-09-10 21:44:09 +0300

    Refactor fetch URL in HomePage component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
   useEffect(() => {
-    fetch("/data.json")
+    fetch("/src/data.json")
       .then((response) => response.json())
       .then((data) => {
         setRecipes(data);
