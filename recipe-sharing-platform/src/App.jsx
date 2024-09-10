b9c6fe383b78e98c3d8562c98ae624e215b765bd
Sandrah <comfortsandrah13@gmail.com>
2024-09-10 20:01:21 +0300

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs text-blue-400">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
