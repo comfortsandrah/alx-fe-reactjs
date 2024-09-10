d0940e642241ef92e7d8dce901459b0837623588
Sandrah <comfortsandrah13@gmail.com>
2024-09-11 00:40:07 +0300

    Refactor recipe list layout in HomePage component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
@@ -24,15 +25,17 @@ const HomePage = () => {
             key={recipe.id}
             className="shadow my-4 rounded-xl bg-orange-200 text-center p-4 md:p-8 hover:shadow-lg"
           >
-            <h2 className="font-semibold md:text-lg underline underline-offset-2 text-green-700 hover:text-green-500 select-none">
-              {recipe.title}
-            </h2>
-            <img
-              src={recipe.image}
-              alt={recipe.title}
-              className="mx-auto rounded-full my-4 hover:scale-110 transition-transform duration-300 ease-in-out sm:w-24 sm:h-24 md:w-36 md:h-36"
-            />
-            <p className="md:text-base sm:text-sm ">{recipe.summary}</p>
+            <Link to={`/recipe/${recipe.id}`}>
+              <h2 className="font-semibold md:text-lg underline underline-offset-2 text-green-700 hover:text-green-500 select-none">
+                {recipe.title}
+              </h2>
+              <img
+                src={recipe.image}
+                alt={recipe.title}
+                className="mx-auto rounded-full my-4 hover:scale-110 transition-transform duration-300 ease-in-out sm:w-24 sm:h-24 md:w-36 md:h-36"
+              />
+              <p className="md:text-base sm:text-sm ">{recipe.summary}</p>
+            </Link>
           </li>
         ))}
       </ul>
