a99a70d155ef7e83b469c24a0e6654d8788b4ad6
Sandrah <comfortsandrah13@gmail.com>
2024-09-24 13:22:47 +0300

    Setting Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./Layout";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Layout />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
