4ea1ad9cee37250ab37ef4c5e11fb84c0be17cb0
Sandrah <comfortsandrah13@gmail.com>
2024-09-24 13:59:35 +0200

    Move Layout component

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Layout from "./Layout";
+import Layout from "./components/Layout";
 
 function App() {
   return (
