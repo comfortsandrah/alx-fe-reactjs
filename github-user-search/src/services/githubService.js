b48e042ff5b370233b63dcb8b0c78b802abd0009
Sandrah <comfortsandrah13@gmail.com>
2024-09-25 22:55:36 +0300

    Refactor githubService.js to remove unnecessary console.log and unused variables

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export function fetchUserData(query) {
     console.log(query)
+    const location = query
+    const minRepos = query
     return axios.get(`https://api.github.com/search/users?q=${query}`)
-
 }
 
